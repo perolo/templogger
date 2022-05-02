087e98473379ad0a879bd91ebcb8e993ca92eabd
Per Olofsson <per.e.olofsson@gmail.com>
2022-05-02 15:25:22 +0200

    Simple test prog

--- templogger.go
+++ templogger.go
@@ -1,118 +1,75 @@
 package main
 
 import (
-	"database/sql"
-	"flag"
 	"fmt"
-	"log"
+	"flag"
+	"runtime"
+	"os"
 	"time"
-	"strings"
-	"strconv"
-	"github.com/magiconair/properties"
-	_ "github.com/mattn/go-sqlite3"
-	"github.com/yryz/ds18b20"
+	"log"
+	"runtime/pprof"
+	"github.com/perolo/ds18b20"
 )
-
-var db *sql.DB
-var cfg Config
-
-type Config struct {
-	DbFile   string `properties:"dbfile"`
-	Interval int    `properties:"interval"`
-	SensorNames string `properties:"sensornames"`
-	ExpectedSensor string `properties:"expectedsensors"`
-	SensorCalibration string `properties:"sensorcalibration"`
-}
 
 func Check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
-func initdb() {
-	var err error
-	db, err = sql.Open("sqlite3", cfg.DbFile)
-	Check(err)
-	statement, prepError := db.Prepare("CREATE TABLE IF NOT EXISTS reading (Id INTEGER PRIMARY KEY AUTOINCREMENT, Sensor INTEGER, Temperature NUMERIC, Datetime DATETIME)")
-	Check(prepError)
-	_, err = statement.Exec()
-	Check(err)
-}
-
-func saveToDatabase(Sensor int, Temperature float64) {
-
-	statement, err := db.Prepare("INSERT INTO reading (Sensor, Temperature, Datetime) VALUES (?,?,CURRENT_TIMESTAMP)")
-	Check(err)
-
-	_, err = statement.Exec(Sensor, Temperature)
-	Check(err)
-}
-
-func main() {
-
-	propPtr := flag.String("prop", "templogger.properties", "a string")
-	flag.Parse()
-
-	p := properties.MustLoadFile(*propPtr, properties.ISO_8859_1)
-
-	if err := p.Decode(&cfg); err != nil {
-		log.Fatal(err)
-	}
+func sens() {
+	fmt.Printf("Hello go! \n")
 	
-	sensorNames := strings.Split(cfg.SensorNames, ",")
-	expectedSensors := strings.Split(cfg.ExpectedSensor, ",")
-	sensorCalibration:= strings.Split(cfg.SensorCalibration, ",")
-	calib := make(map[string]float64)
-
-	initdb()
-
-	if err := db.Ping(); err != nil {
-		log.Fatalf("unable to reach database: %v", err)
-	}
-	fmt.Println("database is reachable")
-
 	sensors, err := ds18b20.Sensors()
 	if err != nil {
 		panic(err)
 	}
-
-	if len(expectedSensors)!= len(sensorCalibration) {
-		fmt.Printf("Expected equal number sensors and calibration, setting calibration to 0")
-		for _, c := range sensors{
-			calib[c] = 0	
-		}
-	} else {
-		for k, c := range sensors{
-			if c != expectedSensors[k] {
-				fmt.Printf("Expected sensor:%s, found sensor: %s ", expectedSensors[k], c)
-			}
-		}
-		for k, c := range sensorCalibration{
-			x, err := strconv.ParseFloat(c, 64)
-			if err != nil {
-				panic(err)
-			}
-			calib[expectedSensors[k]] = x	
-		} 	
-	}
-
-	fmt.Printf("sensor IDs: %v\n", sensorNames)
-	fmt.Printf("sensor IDs: %v\n", sensorCalibration)
-	fmt.Printf("sensor IDs: %v\n", sensors)
-	
- 	/*i:= 1 ; i<10;i++*/ 
-	for{
+	fmt.Printf("Sensors: %s\n", sensors)
+ 	
+	for i:= 1 ; i<10;i++ {
 		//theTime := time.Now().Format("2006-01-02-15:04:05")
 		for key, sensor := range sensors {
 			//fmt.Printf("Time: %s\n", theTime)
 			t, err := ds18b20.Temperature(sensor)
-			t = t - calib[sensor]
 			if err == nil {
-				//fmt.Printf("sensor: %s key: %v temperature: %.2f°C \n", sensor, key, t)
-				saveToDatabase(key, t)
+				fmt.Printf("sensor: %s key: %v temperature: %.2f°C \n", sensor, key, t)
+			} else {
+				fmt.Printf("sensor: %s key: %v err:%s \n", sensor, key, err)
 			}
 		}
-		time.Sleep(time.Duration(cfg.Interval) * time.Second)
+		time.Sleep(time.Duration(time.Second))
 	}
+
+}
+
+func main() {
+	flag.Parse()
+    if *cpuprofile != "" {
+        f, err := os.Create(*cpuprofile)
+        if err != nil {
+            log.Fatal("could not create CPU profile: ", err)
+        }
+        defer f.Close() // error handling omitted for example
+        if err := pprof.StartCPUProfile(f); err != nil {
+            log.Fatal("could not start CPU profile: ", err)
+        }
+        defer pprof.StopCPUProfile()
+    }
+	
+	sens()
+
+    if *memprofile != "" {
+        f, err := os.Create(*memprofile)
+        if err != nil {
+            log.Fatal("could not create memory profile: ", err)
+        }
+        defer f.Close() // error handling omitted for example
+        runtime.GC() // get up-to-date statistics
+        if err := pprof.WriteHeapProfile(f); err != nil {
+            log.Fatal("could not write memory profile: ", err)
+        }
+    }
+
 }
